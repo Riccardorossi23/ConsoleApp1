30ebf329e18c1a1181d48e9680e0d24bd72221c2
Riccardorossi23 <55913260+Riccardorossi23@users.noreply.github.com>
2019-11-21 18:39:54 +0100

    dimmi perchè...

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // int num=2147483648;
+           int num=-2147483648;
+            //int num = -31;
+            Console.WriteLine(Convert.ToString(num, 2));
+            Console.ReadLine();
+        }
+    }
+}
+ 
+
